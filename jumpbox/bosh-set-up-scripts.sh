93c46e5fb68102b83c9917725bf198d752d3ad1f
mattcosgrove <mcosgrove@pivotal.io>
2016-12-03 23:56:19 +0000

    Improve bosh director set up scripts

--- jumpbox/bosh-set-up-scripts.sh
+++ jumpbox/bosh-set-up-scripts.sh
@@ -9,8 +9,20 @@ self_dir=$(dirname "${BASH_SOURCE[0]}")
 source $self_dir/shared-variables.sh
 
 echo $($ssh_bastion "mkdir -p $files_dir")
-echo $(cat $self_dir/shared-variables.sh | $ssh_bastion "cat > ~/shared-variables.sh")
-echo $(cat $self_dir/remote/manifest.yml.erb | $ssh_bastion "cat > ~/$files_dir/manifest.yml.erb")
+printf '%s\n' "Directory $files_dir is created"
 
-echo $($ssh_bastion $env_vars 'bash -s' < $self_dir/remote/bosh-set-up.sh)
+shared_variables="$self_dir/shared-variables.sh"
+shared_variables_dest="~/shared-variables.sh"
+printf '%s\n' "Uploading $shared_variables to $shared_variables_dest"
+
+manifest_template="$self_dir/remote/manifest.yml.erb"
+manifest_template_dest="~/$files_dir/manifest.yml.erb"
+printf '%s\n' "Uploading $manifest_template to $manifest_template_dest"
+echo $(cat $shared_variables | $ssh_bastion "cat > $shared_variables_dest")
+$ssh_bastion "echo export $env_vars >> $shared_variables_dest"
+echo $(cat $manifest_template | $ssh_bastion "cat > $manifest_template_dest")
+
+exec_script="$self_dir/remote/bosh-set-up.sh"
+printf '%s\n' "Executing script $exec_script"
+cat $exec_script | $ssh_bastion
 
--- jumpbox/remote/bosh-set-up.sh
+++ jumpbox/remote/bosh-set-up.sh
@@ -2,18 +2,16 @@
 
 set -eu
 
+source ~/shared-variables.sh
 : ${GCP_CONCOURSE_ZONE_2:?"Need to set GCP_CONCOURSE_ZONE_2 non-empty"}
-
-self_dir=$(dirname "${BASH_SOURCE[0]}")
 
 if [ ! -f ~/.ssh/bosh ]; then
   ssh-keygen -t rsa -f ~/.ssh/bosh -C bosh -P ""
 fi
-
-echo Setting env vars
+printf '%s\n' "Setting env vars...."
 export ssh_key_path=$HOME/.ssh/bosh
 zone=$(curl -sSH "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone)
-echo Zone = $zone
+printf '%s\n\n'  "Zone = $zone"
 export zone=${zone##*/}
 export region=${zone%-*}
 gcloud config set compute/zone ${zone}
@@ -23,8 +21,7 @@ export project_id=`curl -s -H "Metadata-Flavor: Google" http://metadata.google.i
 export zone2=$GCP_CONCOURSE_ZONE_2
 gcloud config list
 
-echo $self_dir
-source ~/shared-variables.sh
-echo $files_dir
-cd google-bosh-director
+pushd $files_dir
 erb manifest.yml.erb > manifest.yml
+popd
+printf '%s\n' "Successfully generated manifest in $files_dir"
